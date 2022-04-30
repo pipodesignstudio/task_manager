dbea8755efbef92b5ab4742adc84a2352d543f56
Pipo Studio <pipodesignstudio@gmail.com>
2022-04-30 20:03:26 +0200

    ok

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:task_manager/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -10,18 +10,65 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final authProvider = Provider.of<AuthProvider>(context);
 
-    // Future.delayed(const Duration(milliseconds: 1500), () async {
-    //   if (authProvider.authStatus == AuthStatus.notAuthenticated) {
-    //     Navigator.pushReplacementNamed(context, 'launch');
-    //   }
-    //   if (authProvider.authStatus == AuthStatus.authenticated) {
-    //     Navigator.pushReplacementNamed(context, 'home');
-    //   }
-    // });
+    Future.delayed(const Duration(milliseconds: 1000), () async {
+      if (authProvider.authStatus == AuthStatus.notAuthenticated) {
+        Navigator.pushReplacementNamed(context, 'launch');
+      }
+      if (authProvider.authStatus == AuthStatus.authenticated) {
+        Navigator.pushReplacementNamed(context, 'home');
+      }
+    });
 
     return Scaffold(
-      body:
-          Center(child: Lottie.asset('assets/lottie/loading.json', width: 150)),
-    );
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          LogoContainer(),
+          SizedBox(
+            height: 20,
+          ),
+          CupertinoActivityIndicator(),
+          SizedBox(
+            height: 150,
+          )
+        ],
+      ),
+    ));
+  }
+}
+
+class LogoContainer extends StatelessWidget {
+  const LogoContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(
+                  color: Color.fromRGBO(16, 24, 40, .08),
+                  blurRadius: 16,
+                  spreadRadius: -4,
+                  offset: Offset(0, 12)),
+              BoxShadow(
+                  color: Color.fromRGBO(16, 24, 40, .08),
+                  blurRadius: 16,
+                  spreadRadius: -4,
+                  offset: Offset(0, 4)),
+            ]),
+        child: Center(
+          child: Image.asset(
+            'assets/img/task_logo.png',
+            fit: BoxFit.contain,
+          ),
+        ));
   }
 }
---
+++ lib/shared/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color headings = Color(0xFF1D2939);
+  static const Color body = Color(0xFF667085);
+  static const Color primary = Color(0xFF3C6879);
+  static const Color secondary = Color(0xFFC9D7E2);
+  static const Color gray300 = Color(0xFFD0D5DD);
+}
