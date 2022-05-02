fe675489ddeb3a4783692dcd08c835ef72cf5c39
Pipo Studio <pipodesignstudio@gmail.com>
2022-05-02 16:01:25 +0200

    ok

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          //TODO:
+        ],
+      ),
+    );
+  }
+}
--- lib/shared/custom_inputs.dart
+++ lib/shared/custom_inputs.dart
@@ -18,14 +18,14 @@ class InputDecorations {
         focusColor: CustomColors.body,
         errorMaxLines: 1,
         errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: CustomColors.gray300, width: 2.0),
+          borderSide: const BorderSide(color: CustomColors.gray300, width: 2.0),
           borderRadius: BorderRadius.circular(5.0),
         ),
         errorStyle: GoogleFonts.workSans(color: CustomColors.primary),
         labelText: labelText,
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
-        labelStyle: TextStyle(color: CustomColors.body),
+        labelStyle: const TextStyle(color: CustomColors.body),
         prefixIcon: prefixIcon != null
             ? Icon(prefixIcon, color: CustomColors.body)
             : null,
